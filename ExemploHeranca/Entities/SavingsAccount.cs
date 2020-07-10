744c2a34f95976334eb5f5a53b422a4f081eb5cf
Raphael Fernandes <holyfernandes@yahoo.co.uk>
2020-07-10 16:45:05 -0300

    Usando conceito de upcasting e downcasting.

---
+++ ExemploHeranca/Entities/SavingsAccount.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ExemploHeranca.Entities
+{
+    class SavingsAccount :Account //extende da classe Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+
+        }
+        public SavingsAccount(int number, string holder, double balance, double interestRate) 
+            :base(number, holder, balance) //aprovitando os parâmetros de Account
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            //Este método atualiza a taxa de juros em cima do saldo
+            Balance += Balance * InterestRate;
+        }
+    }
+}
