4c1eebebcfcd614188931026d54d299d206c6847
Raphael Fernandes <holyfernandes@yahoo.co.uk>
2020-07-10 10:32:10 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploHeranca/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExemploHeranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
